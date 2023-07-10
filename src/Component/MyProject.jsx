b2db2d66ad17f50965f71e9edd9901df93cbf587
AbhinawKumarDeonath <abhinawdeonath30@gmail.com>
2023-07-10 22:23:53 +0530

    added dependecies

--- src/Component/MyProject.jsx
+++ src/Component/MyProject.jsx
@@ -31,7 +31,7 @@ function MyProject() {
       {projectDetail.map((items, i) => (
         <div className="projectBack" style={{backgroundImage:`url(${items.backImage})` , backgroundRepeat:'no-repeat',backgroundSize: "cover"}}>
           <div className="projectBox">
-            <a href={items.webSite} className="projectLink" target="_blank">
+            <a href={items.webSite} rel="noreferrer"  className="projectLink" target="_blank">
               <BiLink />
             </a>
           </div>
